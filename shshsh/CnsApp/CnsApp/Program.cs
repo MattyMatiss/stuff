148554d5471d273f9b1439560bae749b214b3a4d
Matty <mini_homa@mail.ru>
2017-03-11 12:55:04 +0300

    sh

---
+++ shshsh/CnsApp/CnsApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CnsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.Version);
+                
+            Console.ReadLine();
+        }
+    }
+}
